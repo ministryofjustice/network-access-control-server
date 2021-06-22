eedcfc2b48c842796126e2136371138f7179c1a0
Yusuf <yusuf@madetech.com>
2021-06-22 14:57:10 +0100

    Add test command to pipeline

--- test/test_eap.sh
+++ test/test_eap.sh
@@ -3,13 +3,13 @@
 set -ex
 
 test_eap_tls() {
-  eapol_test -r0 -t3 -c eapol_test_tls.conf -a 10.5.0.5 -s testing \
+  eapol_test -r0 -t3 -c /test/eapol_test_tls.conf -a 10.5.0.5 -s testing \
   -M 00:11:22:33:44:55 \
   -N30:s:zzzzzzzzzzz #custom attr Called-Station-Id 
 }
 
 test_eap_tls_ttls() {
-  eapol_test -r0 -t3 -c eapol_test_tls_ttls.conf -a 10.5.0.5 -s testing \
+  eapol_test -r0 -t3 -c /test/eapol_test_tls_ttls.conf -a 10.5.0.5 -s testing \
   -M 00:11:22:33:44:55 \
   -N30:s:zzzzzzzzzzz #custom attr Called-Station-Id 
 }
