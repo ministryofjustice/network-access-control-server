e092101730be857c9f2b8fe1ef343fdc488426b3
Yusuf <yusuf@madetech.com>
2021-06-22 14:51:35 +0100

    Refactor: Remove redundant eapol tests

--- test/test_eap.sh
+++ test/test_eap.sh
@@ -2,14 +2,8 @@
 
 set -ex
 
-test_eap_ttls() {
-  eapol_test -r0 -t3 -c eapol_test_ttls.conf -a 10.5.0.5 -s testing -p1812 \
-  -M 00:11:22:33:44:55 \
-  -N30:s:zzzzzzzzzzz #custom attr Called-Station-Id
-}
-
 test_eap_tls() {
-  eapol_test -r0 -t3 -c eapol_test.conf -a 10.5.0.5 -s testing \
+  eapol_test -r0 -t3 -c eapol_test_tls.conf -a 10.5.0.5 -s testing \
   -M 00:11:22:33:44:55 \
   -N30:s:zzzzzzzzzzz #custom attr Called-Station-Id 
 }
@@ -22,7 +16,6 @@ test_eap_tls_ttls() {
 
 main() {
   test_eap_tls
-  test_eap_ttls
   test_eap_tls_ttls
 }
 
