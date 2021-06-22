0368feb28e0178b863b16d7a6fc3c3703a7967ef
Emile <emile.swarts@digital.justice.gov.uk>
2021-06-22 10:41:53 +0100

    Assume role into correct target AWS account for deployments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+assume_deploy_role() {
+  TEMP_ROLE=`aws sts assume-role --role-arn $ROLE_ARN --role-session-name ci-dhcp-deploy-$CODEBUILD_BUILD_NUMBER`
+  export AWS_ACCESS_KEY_ID=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.AccessKeyId')
+  export AWS_SECRET_ACCESS_KEY=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SecretAccessKey')
+  export AWS_SESSION_TOKEN=$(echo "${TEMP_ROLE}" | jq -r '.Credentials.SessionToken')
+}
+
 deploy() {
   cluster_name=$( jq -r '.radius.ecs.cluster_name' <<< "${TERRAFORM_OUTPUTS}" )
   service_name=$( jq -r '.radius.ecs.service_name' <<< "${TERRAFORM_OUTPUTS}" )
@@ -13,6 +20,7 @@ deploy() {
 }
 
 main() {
+  assume_deploy_role
   deploy
 }
 
