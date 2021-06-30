34c33f4672415e4ee15fc341d3f14884eee537ea
MichaelCullenMadeTech <michael.cullen@madetech.com>
2021-06-30 15:16:47 +0100

    Updateing authenticate-docker script REGISTRY_URL to SHARED_SERVICES_ACCOUNT_ID as the pipelines failed

--- scripts/authenticate_docker.sh
+++ scripts/authenticate_docker.sh
@@ -2,4 +2,4 @@
 
 set -eu pipefail
 
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${REGISTRY_URL}
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${SHARED_SERVICES_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
