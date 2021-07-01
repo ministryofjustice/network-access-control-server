33438c907c708436b3112ae3b49aed33d5caa2b1
MichaelCullenMadeTech <michael.cullen@madetech.com>
2021-07-01 11:11:03 +0100

    Change Local Development variable Startup failed of the server due to unfound variable. Changed deployment script to be correctly named.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,10 +9,10 @@ inject_db_credentials() {
 }
 
 fetch_certificates() {
-    if [[ -z "$LOCAL_DEVELOPMENT" ]]; then
-      aws s3 sync s3://${RADIUS_CERTIFICATE_BUCKET_NAME} /etc/raddb/certs/
-    else
+    if [ "$LOCAL_DEVELOPMENT" == "true" ]; then
       cp -pr ./certs/* /etc/raddb/certs/
+    else
+      aws s3 sync s3://${RADIUS_CERTIFICATE_BUCKET_NAME} /etc/raddb/certs/
     fi
 }
 
