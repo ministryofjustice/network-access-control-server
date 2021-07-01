556abe4568a42769b1ddfb9ee47004f83c3ff418
MichaelCullenMadeTech <michael.cullen@madetech.com>
2021-07-01 10:33:15 +0100

    Added echos for debugging in publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,12 +4,15 @@ set -e
 
 push_image() {
     aws ecr get-login-password | docker login --username AWS --password-stdin ${REGISTRY_URL}
-    docker tag radius:latest $1
+    echo ${TERRAFORM_OUTPUTS}
+    echo $1
+    echo $2
+    docker tag $2:latest $1
     docker push $1:latest
 }
 
 radius_repository_url=$( jq -r '.radius.ecr.repository_url' <<< "${TERRAFORM_OUTPUTS}" )
 nginx_repository_url=$( jq -r '.radius.ecr.nginx_repository_url' <<< "${TERRAFORM_OUTPUTS}" )
 
-push_image $radius_repository_name
-push_image $nginx_repository_name
+push_image $radius_repository_name "radius"
+push_image $nginx_repository_name "nginx"
