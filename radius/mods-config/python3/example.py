385c62dd3ed6af1ec8102400d70326f3f8fd247c
MichaelCullenMadeTech <michael.cullen@madetech.com>
2021-07-29 14:27:17 +0100

    Finally all grouped and working.

--- radius/mods-config/python3/example.py
+++ radius/mods-config/python3/example.py
@@ -7,6 +7,8 @@
 
 import radiusd
 from pymysql import connect, cursors
+from itertools import groupby
+from operator import itemgetter
 
 def instantiate(p):
     print("*** instantiate ***")
@@ -21,31 +23,28 @@ def post_auth(p):
     __site = 'test_client'
 
     with connection.cursor() as cursor:
-        rules_sql = "SELECT `policy`.`policy`, `request_key`, `request_operator`, `request_value` FROM `rules` INNER JOIN `policy` ON `policy`.`policy` = `rules`.`policy` WHERE `policy`.`shortname`=%s;"
-        cursor.execute(rules_sql, (__site,))
-        rules_results = cursor.fetchall()
-        print(rules_results)
-
-        policy_name = ""
+        rules_sql = "SELECT `policy`.`policy_id`, `request_key`, `request_operator`, `request_value` FROM `rules` INNER JOIN `policy` ON `policy`.`policy_id` = `rules`.`policy_id` WHERE `policy`.`shortname`=%s ORDER BY `policy`.`policy_id`;"
+        
+        policy_id = 0
         rules_value_match = 0
         payload_dict = dict(p)
+        cursor.execute(rules_sql, (__site,))
 
-        for result in rules_results:
-            if result['request_operator'] == "==" and result['request_value'] == payload_dict[result['request_key']]:
-                    rules_value_match += 1
-            elif result['request_operator'] == "CONTAINS" and result['request_value'] in payload_dict[result['request_key']]:
-                    rules_value_match += 1 
+        for policy_id, rules in groupby(cursor, lambda row: row['policy_id']):
+            for rule in rules:
+                if rule['request_operator'] == "==" and rule['request_value'] == payload_dict[rule['request_key']]:
+                        rules_value_match += 1
+                elif rule['request_operator'] == "CONTAINS" and rule['request_value'] in payload_dict[rule['request_key']]:
+                        rules_value_match += 1 
+                if rules_value_match == len(list(rules)):
+                    policy_id = rule['policy_id']
 
-            if rules_value_match == len(rules_results):
-                policy_name = result['policy']
-
-        if policy_name != "":
-            reponses_sql = "SELECT `response_key`, `response_value` FROM `responses` WHERE `policy`=%s"
-            cursor.execute(reponses_sql, (policy_name,))
+        if policy_id != 0:
+            reponses_sql = "SELECT `response_key`, `response_value` FROM `responses` WHERE `policy_id`=%s"
+            cursor.execute(reponses_sql, (policy_id,))
             responses_results = cursor.fetchall()
 
             reply_list = [(response['response_key'], response['response_value']) for response in responses_results]
-            print(tuple(reply_list))
 
             update_dict = {
                 "reply" : (
