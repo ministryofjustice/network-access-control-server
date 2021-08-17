55c4532b17be5bfa277929ee54ed0d028581c955
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-17 15:19:06 +0100

    Disable TTY when calling docker-compose exec

--- test/scripts/generate_certs.sh
+++ test/scripts/generate_certs.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
-docker-compose -f docker-compose.yml stop certgenerator
-docker-compose -f docker-compose.yml up --build -d certgenerator
-docker-compose -f docker-compose.yml exec certgenerator make
-docker compose cp certgenerator:/etc/raddb/certs/ ./test
+DOCKER_COMPOSE = docker-compose -f docker-compose.yml
+
+$(DOCKER_COMPOSE) stop certgenerator
+$(DOCKER_COMPOSE) up --build -d certgenerator
+$(DOCKER_COMPOSE) exec -T certgenerator make
+$(DOCKER_COMPOSE) cp certgenerator:/etc/raddb/certs/ ./test
 cat ./test/certs/server.key >> ./test/certs/server.pem
 cat ./test/certs/ca.key >> ./test/certs/ca.pem
 cat ./test/certs/client.key >> ./test/certs/client.pem
