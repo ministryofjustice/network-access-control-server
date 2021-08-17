031f8168bf785021087b1f8b2fa05b6d42427245
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-17 15:24:13 +0100

    Print docker-compose processes

--- test/scripts/generate_certs.sh
+++ test/scripts/generate_certs.sh
@@ -4,8 +4,9 @@ set -ex
 
 docker-compose stop certgenerator
 docker-compose up --build -d certgenerator
-docker-compose exec -T certgenerator make
-docker-compose cp certgenerator:/etc/raddb/certs/ ./test
+docker-compose -f docker-compose.yml ps
+docker-compose -f docker-compose.yml exec -T certgenerator make
+docker compose cp certgenerator:/etc/raddb/certs/ ./test
 cat ./test/certs/server.key >> ./test/certs/server.pem
 cat ./test/certs/ca.key >> ./test/certs/ca.pem
 cat ./test/certs/client.key >> ./test/certs/client.pem
