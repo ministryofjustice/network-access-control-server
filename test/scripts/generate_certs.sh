e79e83b5dd3821bfa64a84d52a0710a0f8fc9745
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-17 15:48:06 +0100

    Wait for certs to be generated
    This container is spun up in the background

--- test/scripts/generate_certs.sh
+++ test/scripts/generate_certs.sh
@@ -2,10 +2,21 @@
 
 set -ex
 
+wait_for_container_to_be_ready() {
+    printf "Waiting for container to be ready"
+    until docker exec ${certgenerator_id} ls -al /etc/raddb/certs/server.pem
+    do
+        printf "."
+        sleep 1
+    done
+    printf "\n"
+}
+
 docker-compose stop certgenerator
 docker-compose up --build -d certgenerator
 certgenerator_id=$(docker ps -aqf "name=certgenerator")
 docker exec ${certgenerator_id} make
+wait_for_container_to_be_ready
 docker cp ${certgenerator_id}:/etc/raddb/certs/ ./test
 cat ./test/certs/server.key >> ./test/certs/server.pem
 cat ./test/certs/ca.key >> ./test/certs/ca.pem
