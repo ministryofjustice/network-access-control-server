009cd74e64317853d519701cf5cf74dcf302e61e
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-17 15:03:11 +0100

    Mkdir -p if the directory exists

--- test/scripts/generate_certs.sh
+++ test/scripts/generate_certs.sh
@@ -9,4 +9,4 @@ docker compose cp certgenerator:/etc/raddb/certs/ ./test
 cat ./test/certs/server.key >> ./test/certs/server.pem
 cat ./test/certs/ca.key >> ./test/certs/ca.pem
 cat ./test/certs/client.key >> ./test/certs/client.pem
-mkdir ./test/certs/radsec && cp -pr ./test/certs/server.pem ./test/certs/ca.pem ./test/certs/radsec/
+mkdir -p ./test/certs/radsec && cp -pr ./test/certs/server.pem ./test/certs/ca.pem ./test/certs/radsec/
