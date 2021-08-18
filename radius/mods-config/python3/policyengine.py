39620bf1cb55179051ee4e0da07acb6554a31cf5
MichaelCullenMadeTech <michael.cullen@madetech.com>
2021-08-18 15:44:23 +0100

    Refactor to keep cursor in main function Fails otherwise due to cursor mismatch, and also performance means it's best to group via cursor

--- radius/mods-config/python3/policyengine.py
+++ radius/mods-config/python3/policyengine.py
@@ -13,52 +13,54 @@ def post_auth(p):
 
     with connection.cursor() as cursor:
         payload_dict = dict(p)
-        _site = payload_dict['Client-Shortname']
-        
-        rules_sql = "SELECT `policy`.`policy_id`, `request_key`, `request_operator`, `request_value` FROM `rules` INNER JOIN `policy` ON `policy`.`policy_id` = `rules`.`policy_id` WHERE `policy`.`shortname`=%s ORDER BY `policy`.`policy_id`;"
-        
         policy_id = 0
-        cursor.execute(rules_sql, (_site,))
+
+        rules_sql = "SELECT `policy`.`policy_id`, `request_key`, `request_operator`, `request_value` " \
+            "FROM `rules` " \
+            "INNER JOIN `policy` ON `policy`.`policy_id` = `rules`.`policy_id` " \
+            "INNER JOIN (SELECT count(*) amount_of_rules, `policy_id` FROM `rules` GROUP BY `policy_id`) `rules_count` ON `rules_count`.`policy_id` = `policy`.`policy_id`" \
+            "WHERE `policy`.`shortname`=%s " \
+            "ORDER BY `rules_count`.`amount_of_rules` DESC, `policy`.`policy_id`;"
+        cursor.execute(rules_sql, (payload_dict['Client-Shortname'],))
 
         for _policy_id, rules in groupby(cursor, lambda row: row['policy_id']):
             rules_value_match = 0
             for rule in rules:
                 if rule['request_operator'] == "==" and rule['request_value'] == payload_dict[rule['request_key']]:
-                        rules_value_match += 1
+                    rules_value_match += 1
                 elif rule['request_operator'] == "CONTAINS" and rule['request_value'] in payload_dict[rule['request_key']]:
-                        rules_value_match += 1 
+                    rules_value_match += 1 
 
                 if rules_value_match == len(list(rules)):
                     policy_id = rule['policy_id']
 
-        update_dict = {}
         reply_list = ()
         if policy_id != 0:
-            reply_list = main_policy(cursor, policy_id)
+            reply_list = main_policy_responses(cursor, policy_id)
         else:
-            reply_list = fallback_policy(cursor, policy_id)
+            reply_list = fallback_policy_responses(cursor, policy_id)
 
         update_dict = {
             "reply" : (
-                tuple(reply_list)
+                reply_list
             )
         }
             
         connection.close()
         return radiusd.RLM_MODULE_OK, update_dict
 
-def main_policy(cursor, policy_id):
+def main_policy_responses(cursor, policy_id):
     reponses_sql = "SELECT `response_key`, `response_value` FROM `responses` WHERE `policy_id`=%s"
     cursor.execute(reponses_sql, (policy_id,))
     responses_results = cursor.fetchall()
-    return group_replies(responses_results)
+    return group_responses(responses_results)
 
-def fallback_policy(cursor, policy_id):
+def fallback_policy_responses(cursor, policy_id):
     fallback_sql = "SELECT `response_key`, `response_value` FROM `responses` INNER JOIN `policy` ON `policy`.`policy_id` = `responses`.`policy_id` WHERE `policy`.`shortname`=%s AND `policy`.`fallback`=1"
     cursor.execute(fallback_sql, (policy_id,))
     responses_results = cursor.fetchall()              
-    return group_replies(responses_results)
+    return group_responses(responses_results)
 
-def group_replies(responses_results):
+def group_responses(responses_results):
     reply_list = [(response['response_key'], response['response_value']) for response in responses_results]
-    return reply_list
+    return tuple(reply_list)
