f52ba47fbbe020e457b1bfcf93bfc8f039ae8305
MichaelCullenMadeTech <michael.cullen@madetech.com>
2021-08-19 10:36:13 +0100

    Remove SQL test data.

--- db/init.sql
+++ db/init.sql
@@ -18,26 +18,3 @@ CREATE TABLE `policy` (
   `policy_id` int(11) DEFAULT NULL,
   `fallback` bit DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO `policy` (policy, shortname, policy_id, fallback) 
-VALUES('test-policy-name', 'test_client', 1, 0);
-
-INSERT INTO `policy` (policy, shortname, policy_id, fallback)
-VALUES('fallback-policy', 'test_client', 2, 1);
-
-INSERT INTO radius.responses(response_key, response_value, policy_id)
-VALUES('Tunnel-Type', 'VLAN', 1);
-INSERT INTO radius.responses(response_key, response_value, policy_id)
-VALUES('Tunnel-Medium-Type', 'IEEE-802', 1);
-INSERT INTO radius.responses(response_key, response_value, policy_id)
-VALUES('Reply-Message', 'The VLAN is being assigned by Python!', 1);
-INSERT INTO radius.responses(response_key, response_value, policy_id)
-VALUES('Tunnel-Private-Group-Id', '1234', 1);
-INSERT INTO radius.responses(response_key, response_value, policy_id)
-VALUES('Reply-Message', 'This is the fallback', 2);
-
-
-INSERT INTO radius.rules(request_key, request_operator, request_value, policy_id)
-VALUES('User-Name', '==', 'MoJ Auth Client', 1);
-INSERT INTO radius.rules(request_key, request_operator, request_value, policy_id)
-VALUES('Service-Type', '==', 'Framed-User', 1);
