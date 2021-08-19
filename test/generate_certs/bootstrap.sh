f653d424799d31071455b423cd2d3a606eab1235
Emile <emile.swarts@digital.justice.gov.uk>
2021-08-19 09:08:23 +0100

    Remove unused Radius files
    Clean up test setup scripts and remove unneeded logging

--- test/generate_certs/bootstrap.sh
+++ test/generate_certs/bootstrap.sh
@@ -2,11 +2,21 @@
 
 set -e
 
-make 
+generate_certs() {
+  make
+}
 
-if ! [ -f /etc/raddb/certs/revoked_client.pem ]; then
+generate_revoked_client_certs() {
+  if ! [ -f /etc/raddb/certs/revoked_client.pem ]; then
     openssl req -batch -passin pass:"whatever" -config client_crl.cnf -new -nodes -keyout client_crl.key -out client_crl.csr
     openssl ca  -batch -passin pass:"whatever" -config ca.cnf -in client_crl.csr -out revoked_client.pem -keyfile ca.key -cert ca.pem
-fi
+  fi
+}
 
-sleep infinity
+main() {
+  generate_certs
+  generate_revoked_client_certs
+  sleep infinity
+}
+
+main
