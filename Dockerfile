b7fc7a5dcf05b1df2c6803c13080ae74f26aa5c2
MichaelCullenMadeTech <79979174+MichaelCullenMadeTech@users.noreply.github.com>
2021-08-20 10:36:17 +0100

    Policy engine python (#21)
    * Interact with sql with python instead of unlang
    
    This can return multiple rows
    
    * Changes so far:
    Tests working and passing again
    Post auth method implemented instead
    Try to send these items back using radiusd command in python now.
    
    * Failing currently due to object not a dict.
    
    * Reply to tuple
    
    * Simplified reply values from python
    Dictionary created instead to match keys
    
    * Working stuff!
    
    * Finally all grouped and working.
    
    * Working script to assign policies based on shortname
    
    * Updated table names correctly.
    
    * Add the AWS cli items back to Dockerfile
    
    * Rules value match should be reset every check
    
    * Update init.sql and remove sql module
    
    * Dockerfile change
    
    * Update policy engine module
    Tests are still failing weirdly though - will attempt to make sense of why.
    
    * Radsec now hits post_auth
    Init.sql for the DB added
    Logging changed just so we can see the messages when not debugging.
    
    * Refactor to keep cursor in main function
    Fails otherwise due to cursor mismatch, and also performance means it's best to group via cursor
    
    * Extract the rules grouping select
    Ensure we break on most rules policy selected
    We now have the amount of rules to match on, rather than a count, improves performance of enumeration.
    
    * Remove SQL test data.
    
    Co-authored-by: Emile <emile.swarts@digital.justice.gov.uk>

--- Dockerfile
+++ Dockerfile
@@ -7,12 +7,12 @@ ENV PYTHONUNBUFFERED=1
 RUN apk update && apk upgrade && apk --no-cache --update add --virtual \
     build-dependencies gnupg && \
     apk --no-cache add tzdata nettle-dev openssl-dev curl \
-    bash wpa_supplicant make freeradius freeradius-eap \
+    bash wpa_supplicant make freeradius freeradius-python3 freeradius-eap \
     openssl build-base gcc libc-dev \
     mysql mysql-client mysql-dev nginx python3 py3-pip \ 
     && python3 && ln -sf python3 /usr/bin/python \
     && python3 -m ensurepip \
-    && pip3 install --no-cache --upgrade pip setuptools \
+    && pip3 install --no-cache --upgrade pip setuptools py-radius PyMySQL \
     && wget "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" \
     && unzip awscli-bundle.zip \
     && rm awscli-bundle.zip \
@@ -20,7 +20,7 @@ RUN apk update && apk upgrade && apk --no-cache --update add --virtual \
     && rm -r ./awscli-bundle \
     && mkdir -p /tmp/radiusd /etc/raddb && openssl dhparam -out /etc/raddb/dh 1024 \
     && mkdir -p /etc/raddb/certs \
-    && rm -fr /etc/raddb/sites-enabled/*
+    && rm -fr /etc/raddb/sites-enabled/* 
 
 COPY radius /etc/raddb
 COPY ./radius/sites-enabled/ /etc/raddb/sites-enabled
---
+++ radius/mods-config/python3/policyengine.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python3
+
+import radiusd
+from pymysql import connect, cursors
+from itertools import groupby
+
+def post_auth(p):
+    connection = connect(host='db',
+                             user='radius',
+                             password='radius',
+                             database='radius',
+                             cursorclass=cursors.DictCursor)
+
+    with connection.cursor() as cursor:
+        payload_dict = dict(p)
+        policy_id = 0
+        grouped_rules_by_policy(cursor, payload_dict['Client-Shortname'])
+
+        for _policy_id, rules in groupby(cursor, lambda row: row['policy_id']):
+            rules_value_match = 0
+            if policy_id != 0:
+                break
+
+            for index, rule in enumerate(rules):
+                if rule['request_operator'] == "==" and rule['request_value'] == payload_dict[rule['request_key']]:
+                    rules_value_match += 1
+                elif rule['request_operator'] == "CONTAINS" and rule['request_value'] in payload_dict[rule['request_key']]:
+                    rules_value_match += 1 
+                
+                if rules_value_match == rule['amount_of_rules']:
+                    policy_id = rule['policy_id']
+
+        reply_list = ()
+        if policy_id != 0:
+            reply_list = main_policy_responses(cursor, policy_id)
+        else:
+            reply_list = fallback_policy_responses(cursor, policy_id)
+
+        update_dict = {
+            "reply" : (
+                reply_list
+            )
+        }
+            
+        connection.close()
+        return radiusd.RLM_MODULE_OK, update_dict
+
+def grouped_rules_by_policy(cursor, _site):
+    rules_sql = "SELECT `policy`.`policy_id`, `request_key`, `request_operator`, `request_value`, `rules_count`.`amount_of_rules` " \
+            "FROM `rules` " \
+            "INNER JOIN `policy` ON `policy`.`policy_id` = `rules`.`policy_id` " \
+            "INNER JOIN (SELECT count(*) amount_of_rules, `policy_id` FROM `rules` GROUP BY `policy_id`) `rules_count` ON `rules_count`.`policy_id` = `policy`.`policy_id`" \
+            "WHERE `policy`.`shortname`=%s " \
+            "ORDER BY `rules_count`.`amount_of_rules` DESC, `policy`.`policy_id`;"
+    cursor.execute(rules_sql, (_site,))
+
+def main_policy_responses(cursor, policy_id):
+    reponses_sql = "SELECT `response_key`, `response_value` FROM `responses` WHERE `policy_id`=%s"
+    cursor.execute(reponses_sql, (policy_id,))
+    responses_results = cursor.fetchall()
+    return group_responses(responses_results)
+
+def fallback_policy_responses(cursor, policy_id):
+    fallback_sql = "SELECT `response_key`, `response_value` FROM `responses` INNER JOIN `policy` ON `policy`.`policy_id` = `responses`.`policy_id` WHERE `policy`.`shortname`=%s AND `policy`.`fallback`=1"
+    cursor.execute(fallback_sql, (policy_id,))
+    responses_results = cursor.fetchall()              
+    return group_responses(responses_results)
+
+def group_responses(responses_results):
+    reply_list = [(response['response_key'], response['response_value']) for response in responses_results]
+    return tuple(reply_list)
