b63269e8f99ebc5c673c203a3cc97c2cf2e68ed1
MichaelCullenMadeTech <michael.cullen@madetech.com>
2021-09-02 12:44:18 +0100

    Grep out results and ensure we check for the VLAN assignment.

--- test/test_policy_engine.sh
+++ test/test_policy_engine.sh
@@ -12,9 +12,24 @@ test_matching_policy() {
   -N 30:s:00-11-22-33-44-55 
 }
 
+assert_policy_result() {
+  cat /integration-results |grep "Attribute 64 (Tunnel-Type) length=6"
+  cat /integration-results |grep "Value: 0000000d"
+  cat /integration-results |grep "Attribute 65 (Tunnel-Medium-Type) length=6"
+  cat /integration-results |grep "Value: 00000006"
+  cat /integration-results |grep "Attribute 81 (Tunnel-Private-Group-Id) length=5"
+  cat /integration-results |grep "Value: 373737"
+
+  if [ $? == 0 ]; then
+    cat /integration-results
+    exit 1
+  fi
+}
+
 main() {
   setup_test_matching_policy
-  test_matching_policy >> /results
+  test_matching_policy > /integration-results
+  assert_policy_result
 }
 
 main
