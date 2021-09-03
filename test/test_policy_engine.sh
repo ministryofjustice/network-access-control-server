6e033ce4d77e2d0532e56bf2f8619afe8e79d662
Emile <emile.swarts@digital.justice.gov.uk>
2021-09-03 08:41:41 +0100

    Refactor policy engine tests
    We have a set -e at the top of this test script so it will exit
    on any error, no need to exit explicitly.

--- test/test_policy_engine.sh
+++ test/test_policy_engine.sh
@@ -10,33 +10,23 @@ test_matching_policy() {
   eapol_test -r0 -t3 -c /test/config/eapol_test_tls.conf -a 10.5.0.5 -s testing
 }
 
-
 test_fallback_policy() {
   eapol_test -r0 -t3 -c /test/config/eapol_test_tls.conf -a 10.5.0.5 -s testing \
   -N4:x:0x0a090807 # random octet IP address to cause fallback to initiate 
 }
 
-
 assert_policy_result() {
-  cat /integration-results |grep "Attribute 64 (Tunnel-Type) length=6"
-  cat /integration-results |grep "Value: 0000000d"
-  cat /integration-results |grep "Attribute 65 (Tunnel-Medium-Type) length=6"
-  cat /integration-results |grep "Value: 00000006"
-  cat /integration-results |grep "Attribute 81 (Tunnel-Private-Group-Id) length=5"
-  cat /integration-results |grep "Value: 373737"
-
-  if [ $? != 0 ]; then
-    exit $?
-  fi
+  grep "Attribute 64 (Tunnel-Type) length=6" /integration-results
+  grep "Value: 0000000d" /integration-results
+  grep "Attribute 65 (Tunnel-Medium-Type) length=6" /integration-results
+  grep "Value: 00000006" /integration-results
+  grep "Attribute 81 (Tunnel-Private-Group-Id) length=5" /integration-results
+  grep "Value: 373737" /integration-results
 }
 
 assert_fallback_policy_result() {
-  cat /integration-results |grep "Attribute 18 (Reply-Message) length=17"
-  cat /integration-results |grep "Value: 'Fallback Policy'"
-
-  if [ $? != 0 ]; then
-    exit $?
-  fi
+  grep "Attribute 18 (Reply-Message) length=17" /integration-results
+  grep "Value: 'Fallback Policy'" /integration-results
 }
 
 main() {
