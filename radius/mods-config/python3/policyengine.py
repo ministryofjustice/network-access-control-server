fce80f3d2bfdb51db3fa9924dcc7e22a104d1fbf
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2022-05-24 12:41:52 +0100

    :recycle: Refactor Policy Engine Sql

--- radius/mods-config/python3/policyengine.py
+++ radius/mods-config/python3/policyengine.py
@@ -102,36 +102,35 @@ def return_responses_with_policy(cursor, payload_dict, policy_id):
 
 
 def grouped_rules_by_policy(cursor, _site):
-    rules_sql = (
-        "SELECT `policies`.`id` policy_id, `policies`.`name` policy_name, `request_attribute`, `operator`, `value`, `policies`.`rule_count` "
-        "FROM `rules` "
-        "INNER JOIN `policies` ON `policies`.`id` = `rules`.`policy_id` "
-        "INNER JOIN site_policies sp ON sp.policy_id = policies.id "
-        "INNER JOIN sites s ON s.id = sp.site_id "
-        "WHERE `s`.`tag`=%s "
-        "ORDER BY sp.priority;"
-    )
-    cursor.execute(rules_sql, (_site,))
+    rules_sql = f"""
+        SELECT policies.id policy_id, policies.name policy_name, request_attribute, operator, value, policies.rule_count FROM rules
+        INNER JOIN policies ON policies.id = rules.policy_id
+        INNER JOIN site_policies sp ON sp.policy_id = policies.id
+        INNER JOIN sites s ON s.id = sp.site_id
+        WHERE s.tag='{_site}'
+        ORDER BY sp.priority;
+    """
+    cursor.execute(rules_sql)
 
 
 def main_policy_responses(cursor, policy_id):
-    reponses_sql = (
-        "SELECT `response_attribute`, `value` FROM `responses` WHERE `policy_id`=%s"
-    )
-    cursor.execute(reponses_sql, (policy_id,))
+    reponses_sql = f"""
+        SELECT response_attribute, value FROM responses WHERE policy_id='{policy_id}';
+    """
+    cursor.execute(reponses_sql)
     responses_results = cursor.fetchall()
     return group_responses(responses_results)
 
 
 def fallback_policy_responses(cursor, _site):
-    fallback_sql = (
-        "SELECT DISTINCT(`response_attribute`), `value` FROM `responses` "
-        "INNER JOIN `policies` ON `policies`.`id` = `responses`.`policy_id` "
-        "INNER JOIN site_policies sp ON sp.policy_id = policies.id "
-        "INNER JOIN sites s ON s.id = sp.site_id "
-        "WHERE `s`.`tag`=%s AND `policies`.`fallback`=1"
-    )
-    cursor.execute(fallback_sql, (_site,))
+    fallback_sql = f"""
+        SELECT DISTINCT(response_attribute), value FROM responses
+        INNER JOIN policies ON policies.id = responses.policy_id
+        INNER JOIN site_policies sp ON sp.policy_id = policies.id
+        INNER JOIN sites s ON s.id = sp.site_id
+        WHERE s.tag='{_site}' AND policies.fallback=1;
+    """
+    cursor.execute(fallback_sql)
     responses_results = cursor.fetchall()
     return group_responses(responses_results)
 
